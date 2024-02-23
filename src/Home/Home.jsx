713b8ab7b8907cb1bfcdb809fa4862b673044354
AnjalyPrakash <anjalyprakaash@gmail.com>
2024-02-23 15:51:16 +0530

    updated

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../Home/Home.css'
 import logo from '../Images/logo.png'
 import Dropdown from 'react-bootstrap/Dropdown';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
@@ -11,7 +12,7 @@ function Home() {
           <img src={logo} style={{ height: '150px', width: '150px' }} alt="" />
           {/* <h1 className='ms-auto me-5 mt-3 text-warning'><i class="fa-solid fa-circle-user"></i></h1>     */}
           <Dropdown className='ms-auto me-5 mt-3' style={{ height: '50px', width: '50px', borderRadius: '25px' }}>
-          <Dropdown.Toggle className='mt-2 me-2' style={{ backgroundColor: ' rgba(0, 0, 0, 0)', height: '50px', width: '50px', borderRadius: '25px', borderColor: 'rgba(0, 0, 0, 0)',display:'none' }} id="dropdown-basic" >
+          <Dropdown.Toggle className='mt-2 me-2' style={{ backgroundColor: ' rgba(0, 0, 0, 0)', height: '50px', width: '50px', borderRadius: '25px', borderColor: 'rgba(0, 0, 0, 0)'}} id="dropdown-basic" >
           <h1 className=' text-secondary'><i class="fa-solid fa-circle-user"></i></h1>
           </Dropdown.Toggle>
           <Dropdown.Menu>
@@ -22,7 +23,7 @@ function Home() {
         </div>
         <div className='d-flex justify-content-center align-items-center flex-column mt-5' >
           <h1 className='text-light  fw-bold mt-5' >Welcome to the wonder of motherhood.</h1>
-          <button className='btn w-25 text-light border mt-2' style={{ borderRadius: '50px' }}>Get Started</button>
+          <Link to={'/login'} className=' d-flex justify-content-center align-items-center' style={{textDecoration:'none' }}><button className='btn border text-light w-100' >Get Started</button></Link>
         </div>
       </div>
     </>
