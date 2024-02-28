36e13588181f72feb182ffa1cae7f758bdec0079
AnjalyPrakash <anjalyprakaash@gmail.com>
2024-02-28 12:17:45 +0530

    update

--- src/Client/ClientRegister.jsx
+++ src/Client/ClientRegister.jsx
@@ -6,15 +6,19 @@ function ClientRegister() {
         <>
        <div className='client_div' style={{width:'100%',height:'100vh'}}>
         <Row >
-            <Col lg={4}></Col>
-            <Col lg={8} className='mt-5 d-flex justify-content-center align-items-center'>
-                <div className='register_div rounded'   style={{width:'700px',height:'500px',backgroundColor:'yellow'}}>
-                    
-
+            <Col md={4}></Col>
+            <Col md={8} className='mt-3 d-flex justify-content-center align-items-center flex-column'>
+                <div className='register_div mt-5 d-flex justify-content-center align-items-center flex-column'>
+                    <h3>Client Registration</h3>
+                    <input type="text" placeholder='Username' style={{borderRadius:'50px'}} className='form-control mb-3 w-50 mt-5'/>
+                    <input type="email" placeholder='Email Address' style={{borderRadius:'50px'}} className='form-control mb-3 w-50 mt-2'/>
+                    <input type="password" placeholder='Password' style={{borderRadius:'50px'}} className='form-control mb-3 w-50 mt-2'/>
+                    <input type="tel" placeholder='Phone Number' style={{borderRadius:'50px'}} className='form-control mb-3 w-50 mt-2'/>
+                    <textarea className='rounded form-control w-50' style={{borderRadius:'50px'}} placeholder='Address' cols="5" rows="5"></textarea>
+                    <button className='w-50 mt-3 btn btn-danger' style={{borderRadius:'50px'}}>REGISTER</button>
                 </div>
             </Col>
         </Row>
-
        </div>
            
         </>
