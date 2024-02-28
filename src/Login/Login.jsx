9ccaa28fc22f4f705b1f6f0f4f2e43ecb9a72238
Dwadhasikrishna <dwadhasikrishnar@gmail.com>
2024-02-28 11:10:06 +0530

    login_page modified

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -7,189 +7,58 @@ import google from '../Images/google2.png'
 function Login() {
   return (
     <>
-      {/* <div className='login_page' style={{ width: '100%', height: '130vh' }}>
-        <Container >
-          <div className='shadow rounded login_body d-flex justify-content-center align-items-center'>
-            <Row>
-              <Col md={12}>
-                <div >
-                  <Row>
-                    <Col md={6}>
-                      <div className='d-flex  justify-content-center align-items-center'>
-                        <h2 className='mt-4 ms-4' style={{ color: 'white' }}>
-                          HELLO<br>
-                          </br>WELCOME!
-                        </h2>
-                      </div>
-                      <div className='mt-2 d-flex justify-content-center align-items-center'>
-                        <img src="https://media.istockphoto.com/id/1341609914/vector/pregnant-couple-background-vector-illustration-with-a-husband-takes-care-and-hugs-his-wife.jpg?s=612x612&w=0&k=20&c=IZ7k7IktzY_x61KybAj2yBioHTLk6r86jafhB3ExN0E=" style={{ width: '300px', height: '300px', borderRadius: '50%' }} alt="" />
-                      </div>
-                      <div className=' d-flex justify-content-center align-items-center mt-3 ms-3'>
-                        <p style={{ fontWeight: 'bold', color: 'blueviolet' }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, dignissimos amet hic sed iusto illum similique sit, animi quam  </p>
-                      </div>
-                    </Col>
-                    <Col md={6}>
-                      <div className='mt-4 me-3 rounded' style={{ height: '500px', backgroundColor: 'white' }}>
-                        <div className='d-flex justify-content-center align-items-center'>
-                          <h4 className='mt-3' style={{ color: 'blueviolet' }}>Login Your Account</h4>
-                        </div>
-                        <div className='d-flex justify-content-center align-items-center'>
-                          <TextField className='w-75 mt-4' id="standard-basic" type='email' label="Email" variant="standard" />
-                        </div>
-                        <div className='d-flex justify-content-center align-items-center'>
-                          <TextField className='w-75 mt-4' id="standard-basic" type='password' label="Password" variant="standard" />
-                        </div>
-                        <div className='d-flex'>
-                          <input className='ms-5 mt-4' style={{ width: '25px' }} type="checkbox" />
-                          <h6 className='mt-4 ms-3'>Remember?</h6>
-                          <a href="" className='mt-4 ms-5'>forgot password?</a>
-                        </div>
-                        <div className='d-flex justify-content-center align-items-center'>
-                          <button className='btn login_button rounded w-50 mt-4'>Login</button>
-                        </div>
-                        <div className='d-flex justify-content-center align-items-center'>
-                          <p className='mt-3'>or connect with google</p>
-                        </div>
-                        <div className='d-flex justify-content-center align-items-center flex-row '>
-                          <div className='google d-flex justify-content-evenly rounded' style={{ width: '50%'}}>
-                            <img className='ms-4 mt-2 me-1' src={google} style={{ width: '25px'}} alt="" />
-                            <h6 className=' mt-2'>Sign in with google</h6>
-                          </div>
-                        </div>
-                      </div>
-                    </Col>
-                  </Row>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Container>
-      </div> */}
-
-      {/* -------------------------------------------------------- */}
-      {/* <Row>
-     <div className='login_page' style={{width:'100%',height:'130vh'}}>
-      <Row className='mt-5'>
-        <Col md={1}></Col>
-        <Col md={10} className='mt-5 d-flex justify-content-center align-items-center'> 
-          <div className='shadow rounded login_body' style={{width:'75%',height:'550px',backgroundColor:'white'}}>
-            <Row>
-              <Col md={6}>
-                <div className='d-flex  justify-content-center align-items-center'>
-                <h2  className='mt-4 ms-4' style={{color:'white'}}>
-                    HELLO<br>
-                    </br>WELCOME!
-                  </h2>
-                </div>
-                <div className='mt-2 d-flex justify-content-center align-items-center'>
-                  <img src="https://media.istockphoto.com/id/1341609914/vector/pregnant-couple-background-vector-illustration-with-a-husband-takes-care-and-hugs-his-wife.jpg?s=612x612&w=0&k=20&c=IZ7k7IktzY_x61KybAj2yBioHTLk6r86jafhB3ExN0E=" style={{width:'300px',height:'300px',borderRadius:'50%'}} alt="" />
-                </div>
-                <div className=' d-flex justify-content-center align-items-center mt-3 ms-3'>
-                  <p style={{fontWeight:'bold',color:'blueviolet'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, dignissimos amet hic sed iusto illum similique sit, animi quam  </p>
-                </div>
-              </Col>
-         
-              <Col md={6}>
-              <div className='mt-4 me-3 rounded' style={{height:'500px',backgroundColor:'white'}}>
-                <div className='d-flex justify-content-center align-items-center'>
-                  <h4 className='mt-3' style={{color:'blueviolet'}}>Login Your Account</h4>
-                </div>
-                <div className='d-flex justify-content-center align-items-center'>
-                <TextField className='w-75 mt-4' id="standard-basic" type='email' label="Email" variant="standard" />
-                </div>
-                <div className='d-flex justify-content-center align-items-center'>
-                <TextField className='w-75 mt-4' id="standard-basic" type='password' label="Password" variant="standard" />
-                </div>
-                <div className='d-flex'>
-                <input className='ms-5 mt-4' style={{width:'25px'}} type="checkbox"/>
-                <h6 className='mt-4 ms-3'>Remember?</h6>
-                <a href="" className='mt-4 ms-5'>forgot password?</a>
-                </div>
-                <div className='d-flex justify-content-center align-items-center'>
-                  <button className='btn login_button rounded w-50 mt-4'>Login</button>
-                </div>
-                <div className='d-flex justify-content-center align-items-center'>
-                  <p className='mt-3'>or connect with google</p>
-                </div>
-                <div className='d-flex justify-content-center align-items-center flex-row '>
-                  <div className='google d-flex justify-content-evenly rounded' style={{width:'50%',height:'50px'}}>
-                    <img className='ms-4 mt-2 me-1' src={google} style={{width:'25px',height:'25px'}} alt="" />
-                    <h6 className=' mt-2'>Sign in with google</h6>
-                  </div>
-                </div>
-              </div>
-              </Col>
-            </Row>
-
-          </div>
-        </Col>
-        <Col md={1}></Col>
-      </Row>
-   
-     </div>
-    </Row> */}
-
-
-    {/* ------------------------------ */}
-
-    <Row>
-  <div className='login_page'>
+        <Row>
+  <div className='login_page' style={{ width: '100%', minHeight: '100vh' }}>
     <Row className='mt-5'>
-      <Col md={1}></Col>
-      <Col md={10} className='mt-5 d-flex justify-content-center align-items-center'>
-        <div className='shadow rounded login_body p-4' style={{ width: '75%', maxWidth: '1000px', backgroundColor: 'white' }}>
+      <Col lg={1}></Col>
+      <Col lg={10} className='mt-5 d-flex justify-content-center align-items-center'>
+        <div className='shadow rounded login_body' style={{ width: '100%', maxWidth: '800px', backgroundColor: 'white', padding: '20px' }}>
           <Row>
-          <Col md={6} className='order-md-1'>
-              <div className='d-flex  justify-content-center align-items-center'>
-                <h2 className='mt-4 ms-4' style={{ color: 'white' }}>
-                  HELLO<br></br>WELCOME!
-                </h2>
-              </div>
-              <div className='mt-2 d-flex justify-content-center align-items-center'>
-                <img src="https://media.istockphoto.com/id/1341609914/vector/pregnant-couple-background-vector-illustration-with-a-husband-takes-care-and-hugs-his-wife.jpg?s=612x612&w=0&k=20&c=IZ7k7IktzY_x61KybAj2yBioHTLk6r86jafhB3ExN0E=" style={{ maxWidth: '100%', height: 'auto', borderRadius: '50%' }} alt="" />
-              </div>
-              <div className=' d-flex justify-content-center align-items-center mt-3 ms-3'>
-                <p style={{ fontWeight: 'bold', color: 'blueviolet' }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, dignissimos amet hic sed iusto illum similique sit, animi quam </p>
-              </div>
+            <Col lg={6} className='d-flex flex-column justify-content-center align-items-center'>
+              <h2 className='mt-4' style={{ color: 'blueviolet', textAlign: 'center' }}>HELLO<br />WELCOME!</h2>
+              <img src="https://media.istockphoto.com/id/1341609914/vector/pregnant-couple-background-vector-illustration-with-a-husband-takes-care-and-hugs-his-wife.jpg?s=612x612&w=0&k=20&c=IZ7k7IktzY_x61KybAj2yBioHTLk6r86jafhB3ExN0E=" style={{ width: '200px', height: '200px', borderRadius: '50%', margin: '20px 0' }} alt="" />
+              <p style={{ fontWeight: 'bold', color: 'blueviolet', textAlign: 'center' }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, dignissimos amet hic sed iusto illum similique sit, animi quam</p>
             </Col>
-            <Col md={6} className='order-md-2'>
-              <div className='mt-4 me-3 rounded' style={{ height: '500px', backgroundColor: 'white' }}>
-                <div className='d-flex justify-content-center align-items-center'>
-                  <h4 className='mt-3' style={{ color: 'blueviolet' }}>Login Your Account</h4>
-                </div>
+
+            <Col lg={6} className='mt-4'>
+              <div className='rounded' style={{ backgroundColor: 'white', padding: '20px' }}>
+                <h4 className='mt-3' style={{ color: 'blueviolet', textAlign: 'center' }}>Login Your Account</h4>
                 <div className='d-flex justify-content-center align-items-center'>
                   <TextField className='w-75 mt-4' id="standard-basic" type='email' label="Email" variant="standard" />
                 </div>
                 <div className='d-flex justify-content-center align-items-center'>
                   <TextField className='w-75 mt-4' id="standard-basic" type='password' label="Password" variant="standard" />
                 </div>
-                <div className='d-flex'>
-                  <input className='ms-5 mt-4' style={{ width: '25px' }} type="checkbox" />
-                  <h6 className='mt-4 ms-3'>Remember?</h6>
-                  <a href="" className='mt-4 ms-5'>forgot password?</a>
+                <div className='d-flex align-items-center mt-4'>
+                  <input className='me-2' style={{ width: '20px', height: '20px' }} type="checkbox" />
+                  <label htmlFor="remember" className='me-3'>Remember?</label>
+                  <a href="#" className='me-auto'>Forgot password?</a>
                 </div>
                 <div className='d-flex justify-content-center align-items-center'>
-                  <button className='btn login_button rounded w-50 mt-4'>Login</button>
+                  <button className='btn login_button rounded w-75 mt-4'>Login</button>
+                </div>
+                <div className='d-flex justify-content-center align-items-center mt-3'>
+                  <p>or connect with google</p>
                 </div>
                 <div className='d-flex justify-content-center align-items-center'>
-                  <p className='mt-3'>or connect with google</p>
-                </div>
-                <div className='d-flex justify-content-center align-items-center flex-row '>
-                  <div className='google d-flex justify-content-evenly rounded' style={{ width: '50%', height: '50px' }}>
-                    <img className='ms-4 mt-2 me-1' src={google} style={{ width: '25px', height: '25px' }} alt="" />
-                    <h6 className=' mt-2'>Sign in with google</h6>
+                  <div className='google d-flex justify-content-evenly rounded' style={{ width: '75%', height: '50px', backgroundColor: '#db4437', color: 'white', cursor: 'pointer' }}>
+                    <img className='ms-2' src={google} style={{ width: '25px', height: '25px' }} alt="" />
+                    <h6 className='mt-2'>Sign in with Google</h6>
                   </div>
                 </div>
               </div>
             </Col>
-           
           </Row>
         </div>
       </Col>
-      <Col md={1}></Col>
+      <Col lg={1}></Col>
     </Row>
   </div>
 </Row>
+
+
+   
+    
 
     </>
   )
